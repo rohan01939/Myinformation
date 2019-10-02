3b935ced7be757f2ff830ae03d458a3fd656e5fe
rohan01939 <rohan01939@gmail.com>
2019-10-02 02:17:21 -0400

    about me

---
+++ src/aboutme/Myinformation.java
@@ -0,0 +1,14 @@
+package aboutme;
+
+public class Myinformation {
+public static void main(String [] args) {
+	String aa="Rohan";
+	String ab="I am";
+	String ac="Bangladesh";
+	int bb=24;
+	short bc=2013;
+	
+	System.out.println("My name is "+aa+" and i am "+bb+" years old " +ab+" from "+ac+ " I came here on "+bc
+			);
+}
+}
